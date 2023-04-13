5445ac70dc1007993f1238ae38ee623c2cd76f26
fizzgig <fastzombies@users.noreply.github.com>
2023-04-13 17:21:26 -0500

    Add module0.py

---
+++ module0.py
@@ -0,0 +1,22 @@
+import logging
+
+
+def module0_function0():
+    l = logging.getLogger()
+    l.debug('module0_function0')
+    l.info('module0_function0')
+    #logger.warning('module0_function0')
+    #logger.error('module0_function0')
+    #logger.critical('module0_function0')
+    #logger.fatal('module0_function0')
+    print('module0_function0 plain print\n')
+
+def module0_function1():
+    l = logging.getLogger()
+    l.debug('module0_function1')
+    l.info('module0_function1')
+    #logger.warning('module0_function1')
+    #logger.error('module0_function1')
+    #logger.critical('module0_function1')
+    #logger.fatal('module0_function1')
+    print('module0_function1 plain print\n')
